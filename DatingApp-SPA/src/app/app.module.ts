27be7a46367c1fdcd278a81f66ef74d1721e8476
RoHi <robhizon26@yahoo.com>
2018-12-09 16:15:40 -0500

    End of Section 16

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,41 +1,44 @@
-import { ListsResolver } from "./_resolvers/lists.resolver";
-import { PhotoEditorComponent } from "./members/photo-editor/photo-editor.component";
-import { MemberEditResolver } from "./_resolvers/member-edit.resolver";
-import { MemberListResolver } from "./_resolvers/member-list.resolver";
-import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
-import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
-import { MemberCardComponent } from "./members/member-card/member-card.component";
-import { AlertifyService } from "./_services/alertify.service";
-import { ErrorInterceptorProvider } from "./_services/error.interceptor";
-import { AuthService } from "./_services/auth.service";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-import { AppComponent } from "./app.component";
-import { NavComponent } from "./nav/nav.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import {
   BsDropdownModule,
   TabsModule,
   BsDatepickerModule,
-  ButtonsModule,
-  PaginationModule
+  PaginationModule,
+  ButtonsModule
 } from "ngx-bootstrap";
-import { HomeComponent } from "./home/home.component";
-import { RegisterComponent } from "./register/register.component";
-import { MemberListComponent } from "./members/member-list/member-list.component";
-import { MessagesComponent } from "./messages/messages.component";
 import { RouterModule } from "@angular/router";
-import { appRoutes } from "./routes";
-import { AuthGuard } from "./_guards/auth.guard";
-import { ListsComponent } from "./lists/lists.component";
-import { UserService } from "./_services/user.service";
 import { JwtModule } from "@auth0/angular-jwt";
 import { NgxGalleryModule } from "ngx-gallery";
-import { MemberEditComponent } from "./members/member-edit/member-edit.component";
-import { PreventUnsavedChanges } from "./_guards/prevent-unsaved-changes.guard";
 import { FileUploadModule } from "ng2-file-upload";
 import { TimeAgoPipe } from "time-ago-pipe";
+
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { AuthService } from "./_services/auth.service";
+import { HomeComponent } from "./home/home.component";
+import { RegisterComponent } from "./register/register.component";
+import { ErrorInterceptorProvider } from "./_services/error.interceptor";
+import { AlertifyService } from "./_services/alertify.service";
+import { MemberListComponent } from "./members/member-list/member-list.component";
+import { ListsComponent } from "./lists/lists.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { appRoutes } from "./routes";
+import { AuthGuard } from "./_guards/auth.guard";
+import { UserService } from "./_services/user.service";
+import { MemberCardComponent } from "./members/member-card/member-card.component";
+import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
+import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
+import { MemberListResolver } from "./_resolvers/member-list.resolver";
+import { MemberEditComponent } from "./members/member-edit/member-edit.component";
+import { MemberEditResolver } from "./_resolvers/member-edit.resolver";
+import { PreventUnsavedChanges } from "./_guards/prevent-unsaved-changes.guard";
+import { PhotoEditorComponent } from "./members/photo-editor/photo-editor.component";
+import { ListsResolver } from "./_resolvers/lists.resolver";
+import { MessagesResolver } from "./_resolvers/messages.resolver";
+import { MemberMessagesComponent } from "./members/member-messages/member-messages.component";
 
 export function tokenGetter() {
   return localStorage.getItem("token");
@@ -48,13 +51,14 @@ export function tokenGetter() {
     HomeComponent,
     RegisterComponent,
     MemberListComponent,
-    MessagesComponent,
     ListsComponent,
+    MessagesComponent,
     MemberCardComponent,
     MemberDetailComponent,
     MemberEditComponent,
     PhotoEditorComponent,
-    TimeAgoPipe
+    TimeAgoPipe,
+    MemberMessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -87,7 +91,8 @@ export function tokenGetter() {
     MemberListResolver,
     MemberEditResolver,
     PreventUnsavedChanges,
-    ListsResolver
+    ListsResolver,
+    MessagesResolver
   ],
   bootstrap: [AppComponent]
 })
